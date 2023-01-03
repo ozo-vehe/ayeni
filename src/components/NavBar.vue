6d8b6a11e08a8633140eb191b8ba8b5d2a097e0f
ozo-vehe <johnnie.vehe@gmail.com>
2023-01-03 14:43:04 +0100

    changed logo design

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,14 @@
 <script setup>
-import { reactive } from "vue";
-import behance from "../assets/icons/icon(1).png";
-import medium from "../assets/icons/icon(2).png";
-import twitter from "../assets/icons/icon(3).png";
-import linkedin from "../assets/icons/icon(4).png";
-import dribble from "../assets/icons/icon(5).png";
-
-defineProps({
-  name: String || "Welcome",
-})
+  import { reactive } from "vue";
+  import behance from "../assets/icons/icon(1).png";
+  import medium from "../assets/icons/icon(2).png";
+  import twitter from "../assets/icons/icon(3).png";
+  import linkedin from "../assets/icons/icon(4).png";
+  import dribble from "../assets/icons/icon(5).png";
+  import logo from "../assets/icons/logo.svg"
+  defineProps({
+    name: String || "Welcome",
+  })
 
 const links = reactive([
   {name: "behance", href: "https://www.behance.net/obansajohn", iconLink: behance},
@@ -20,11 +20,11 @@ const links = reactive([
 </script>
 
 <template>
-  <main class="box-border px-20 py-8 flex justify-between">
-    <h1 class="text-dark text-2xl font-bold">{{ name }}</h1>
+  <main class="box-border px-20 py-8 flex flex-wrap gap-y-8 justify-between">
+    <img class="w-50" :src="logo" alt="Logo">
     <ul class="flex gap-10">
       <li v-for="link in links">
-        <a :href="link.href" target="_blank"><img :src="link.iconLink" :alt="link.name" /></a>
+        <a :href="link.href" target="_blank"><img class="w-50" :src="link.iconLink" :alt="link.name" /></a>
       </li>
     </ul>
   </main>
